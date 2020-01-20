2ef948d3c953543a79b9fc511d127602562e05dd
Luis Garcia <lalejandro.1988@gmail.com>
2020-01-19 21:24:10 -0600

    git

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { setTimeout } from 'timers';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre = 'Fernando';
+  nombre2 = 'luis alEJandro garCIA hernANdez';
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: '34',
+      zona: '10'
+    }
+  };
+
+  valorDePromesa = new Promise( ( resolve, reject ) => {
+
+    setTimeout( () => resolve('Llego la data!'), 3500 );
+
+  });
+
+  fecha = new Date();
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+
+registerLocaleData(localeEs);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CapitalizadoPipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
